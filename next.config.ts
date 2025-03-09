7c118dc567021d1661522286906bbbd5ded16e4f
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2025-03-09 20:19:17 +0100

    Added eggepizza

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        domains: ["images.unsplash.com"], // Allow Unsplash images
-    },
 };
 
 export default nextConfig;
