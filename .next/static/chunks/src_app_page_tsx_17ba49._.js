c8a2b033bea4b3b4895112d814f68d93678b01bf
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2025-10-20 11:07:49 +0200

    Add build

---
+++ .next/static/chunks/src_app_page_tsx_17ba49._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_17ba49._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_17ba49._.js",
+  "chunks": [
+    "static/chunks/node_modules_next_3b5af4._.js",
+    "static/chunks/[root of the server]__897d60._.js",
+    "static/chunks/[root of the server]__9941d8._.css"
+  ],
+  "source": "dynamic"
+});
