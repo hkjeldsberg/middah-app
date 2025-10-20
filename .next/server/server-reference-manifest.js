bbe73f199b7e9591839d27bfd30c2b003fafddb6
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2025-10-20 21:24:41 +0200

    Add files

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"paMyVsOlOen0Y624WoG+VWx73AKNNjYrmqw8Zl6pnZo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
